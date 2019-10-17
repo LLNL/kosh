c1d65501c6dc3335c0e9a27aab94cc2a060cd088
Charles Doutriaux <doutriaux1@llnl.gov>
2019-10-17 16:18:52 -0700

    adding array file

---
+++ kosh/arrays.py
@@ -0,0 +1,11 @@
+
+class KoshAxis(object):
+    def __init__(self, id, values):
+        self.id = id
+        self.__values = values
+    def __getitem__(self, key):
+        return self.__values[key]
+    def __setitem__(self, key, value):
+        self.__values[key] = value
+    def __len__(self):
+        return len(self.__values)
