2c09d53ba6da1f0bffda7569d20a50600b89ccb7
Charles Doutriaux <doutriaux1@llnl.gov>
2019-10-25 10:00:20 -0700

    added flake8 tst

--- tests/test_kosh_00.py
+++ tests/test_kosh_00.py
@@ -3,9 +3,10 @@ import os
 import sys
 import shlex
 from subprocess import PIPE, Popen
-from  kosh import KoshStore
+from kosh import KoshStore
 
 kosh_test_sql_file = "kosh_test.sql"
+
 
 class KoshTest(unittest.TestCase):
     def setUp(self):
@@ -14,7 +15,8 @@ class KoshTest(unittest.TestCase):
     def test_connect(self):
         self.init_db()
         if self.engine == "sina":
-            store = KoshStore(engine="sina", username=os.getlogin(), sql='sql', db_path=kosh_test_sql_file)
+            KoshStore(engine="sina", username=os.getlogin(), sql='sql',
+                      db_path=kosh_test_sql_file)
 
     def init_db(self):
         if self.engine == "sina":
@@ -22,7 +24,7 @@ class KoshTest(unittest.TestCase):
             if os.path.exists(kosh_test_sql_file):
                 os.remove(kosh_test_sql_file)
 
-            cmd = "{}/bin/python scripts/init_sina.py --sina_db={}".format(sys.prefix, kosh_test_sql_file)
-            print("EXEC:", cmd)
+            cmd = "{}/bin/python scripts/init_sina.py --sina_db={}".format(
+                sys.prefix, kosh_test_sql_file)
             p = Popen(shlex.split(cmd), stdout=PIPE, stderr=PIPE)
             o, e = p.communicate()
