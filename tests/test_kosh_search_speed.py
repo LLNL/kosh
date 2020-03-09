eaa8f670c336d41df43e35cf728908c34e7a954d
Charles Doutriaux <doutriaux1@llnl.gov>
2020-03-09 15:57:51 -0700

    ok much faster search speed.
    had to actually create in mem db to keep track of datasets added while in sync=False mode, so we do not need to create it with each search

--- tests/test_kosh_search_speed.py
+++ tests/test_kosh_search_speed.py
@@ -1,5 +1,6 @@
 import koshbase
 import time
+import numpy
 
 class TestKoshSearchSpeed(koshbase.KoshTest):
     def testSpeedSearch(self):
@@ -13,10 +14,13 @@ class TestKoshSearchSpeed(koshbase.KoshTest):
             meta[f"D_{chr(i)}"] = str(i)
 
         search_times = []
-        for i in range(200):
+        for i in range(90):
             start = time.time()
-            search = store.search(**meta)
+            search = store.search(ds_only=True,**meta)
             search_times.append(time.time() - start)
-            print(f"Iter: {i} took {search_times[-1]}")
             ds = store.create(metadata=meta)
             ds.associate("/some_path", mime_type="some type")
+        store.sync()
+        a, b = numpy.polyfit(numpy.arange(len(search_times)), numpy.array(search_times), 1)
+        print("A, B:", a, b)
+
