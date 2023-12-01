a32ab90ef11fdaca85bd72e8d6822eaac7888ade
Renee Olson <olson59@llnl.gov>
2023-12-01 11:22:14 -0800

    print inputs

--- kosh/operators/koshClustering.py
+++ kosh/operators/koshClustering.py
@@ -130,10 +130,10 @@ class KoshCluster(KoshOperator):
         # Get the sizes of each kosh dataset
         input_sizes = []
         desc = list(self.describe_entries())
-        breakpoint()
         for i in range(len(inputs)):
             input_sizes.append(desc[i]["size"][0])
-
+        if self.pverbose:
+            print(f"Inputs: {input_sizes}")
         total_sample_size = sum(input_sizes)
 
         # Logical checks for batch and parallel clustering
