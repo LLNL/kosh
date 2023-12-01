f05c18b6c96b0b26d5bc07f27816e4a062e99282
Charles Doutriaux <doutriaux1@llnl.gov>
2023-12-01 10:48:00 -0800

    correct png size

--- scripts/render_logos.py
+++ scripts/render_logos.py
@@ -52,12 +52,8 @@ for svg in svgs:
     with open(svg_name, "w") as f:
         f.write(content)
     png_name = svg_name.replace("svg", "png")
+    cmd = "cairosvg  {} -o {} -W=1035 -H 389".format(svg_name, png_name)
     if not sys.platform.startswith("win"):
-        svg_code = open(svg_name,"r").read()
-        from cairosvg import svg2png
-        svg2png(bytestring=svg_code,write_to=png_name)
-    else:
-        cmd = "inkscape --file {} --export-png {} --export-width=1035".format(
-            svg_name, png_name)
-        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
-        o, e = p.communicate()
+        cmd = shlex.split(cmd)
+    p = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    o, e = p.communicate()
